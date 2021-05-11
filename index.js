625c2a79a0827aafbe3daa98a2aab24eee9a90f4
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-11 09:12:30 -0700

    added mongoose, removed static api

--- index.js
+++ index.js
@@ -3,92 +3,13 @@ const express = require('express'),
 
 const app = express();
 
-let movies = [
-  {
-    title: 'Resident Evil: Retribution',
-		year: '2012',
-    publisher: 'Capcom',
-		director: 'Paul W. S. Anderson'
-  },
-  {
-    title: 'Silent Hill: Revelation',
-		year: '2012',
-    publisher: 'Konami',
-		director: 'M. J. Bassett'
-  },
-  {
-    title: 'Need For Speed',
-		year: '2014',
-    publisher: 'Electronic Arts',
-		director: 'Scott Waugh'
-  },
-	{
-    title: 'Hitman: Agent 47',
-		year: '2015',
-    publisher: 'Square Enix',
-		director: 'Aleksander Bach'
-  },
-  {
-    title: 'Warcraft',
-		year: '2016',
-    publisher: 'Blizzard Entertainment',
-		director: 'Duncan Jones'
-  },
-  {
-    title: 'Assassin\'s Creed',
-		year: '2016',
-    publisher: 'Ubisoft',
-		director: 'Justin Kurzel'
-  },
-	{
-    title: 'Resident Evil: The Final Chapter',
-		year: '2017',
-    publisher: 'Capcom',
-		director: 'Paul W. S. Anderson'
-  },
-  {
-    title: 'Tomb Raider',
-		year: '2018',
-    publisher: 'Square Enix',
-		director: 'Roar Uthaug'
-  },
-  {
-    title: 'Rampage',
-		year: '2018',
-    publisher: 'Midway',
-		director: 'Brad Peyton'
-  },
-	{
-    title: 'Dead Trigger',
-		year: '2019',
-    publisher: 'Madfinger Games',
-		director: 'Mike Cuff'
-  },
-  {
-    title: 'Detective Pikachu',
-		year: '2019',
-    publisher: 'Nintendo',
-		director: 'Rob Letterman'
-  },
-  {
-    title: 'Sonic The Hedgehog',
-		year: '2020',
-    publisher: 'Sega',
-		director: 'Jeff Fowler'
-  },
-	{
-		title: 'Monster Hunter',
-		year: '2020',
-		publisher: 'Capcom',
-		director: 'Paul W. S. Anderson'
-	},
-	{
-		title: 'Mortal Kombat',
-		year: '2021',
-		publisher: 'WB Games',
-		director: 'Simon McQuoid'
-	}
-];
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // GET requests
 app.get('/', (req, res) => {
