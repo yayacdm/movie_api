19ff623045262fd8ad7ae86c807ad58fa297714f
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-12 11:01:09 -0700

    added bcrypt to models.js

--- models.js
+++ models.js
@@ -16,6 +16,8 @@ let movieSchema = mongoose.Schema({
 	Year: String
 });
 
+const bcrypt = require('bcrypt');
+
 let userSchema = mongoose.Schema({
 	Username: {type: String, required: true},
 	Password: {type: String, required: true},
@@ -23,6 +25,14 @@ let userSchema = mongoose.Schema({
 	Birthday: Date,
 	Favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
+
+userSchema.statics.hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.Password);
+};
 
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
