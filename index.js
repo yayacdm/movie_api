0aba8ad3c439461cf2bfed815a44ea8ebd122523
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-12 20:52:32 -0700

    updated listen requirement

--- index.js
+++ index.js
@@ -191,6 +191,7 @@ app.delete('/users/:Username', (req, res) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });
