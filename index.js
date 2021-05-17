0d764db7106838170d561ffd436633c151d6047b
Yaya <82174082+yayacdm@users.noreply.github.com>
2021-05-17 08:57:09 -0700

    removed auth from index

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
 });
 
 // GET all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', function (req, res) => {
   Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
